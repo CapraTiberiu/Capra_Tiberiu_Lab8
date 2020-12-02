3598ac46496a9a80aab2975c4bc0141f722b0afb
Capra Tiberiu <tiberiumc@yahoo.com>
2020-12-02 18:01:53 +0200

    Add project files.

---
+++ Data/Capra_Tiberiu_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Capra_Tiberiu_Lab8.Models;
+
+namespace Capra_Tiberiu_Lab8.Data
+{
+    public class Capra_Tiberiu_Lab8Context : DbContext
+    {
+        public Capra_Tiberiu_Lab8Context (DbContextOptions<Capra_Tiberiu_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Capra_Tiberiu_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Capra_Tiberiu_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Capra_Tiberiu_Lab8.Models.Category> Category { get; set; }
+    }
+}
